a1ba7dfe8a9bc6072c5e88a0494d9cb6f6f1548c
Cristian <cccastanedaa@correo.udistrital.edu.co>
2020-12-22 23:53:20 -0500

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template>
+          <v-list-item :to="{ name: 'home' }">
+            <v-list-item-action>
+              <v-icon> home </v-icon>
+            </v-list-item-action>
+            <v-list-item-title> Inicio </v-list-item-title>
+          </v-list-item>
+        </template>
+
+        <template>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title> Almacén </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{ name: 'categoria' }">
+              <v-list-item-action>
+                <v-icon> table_chart </v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title> Categorías </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+
+            <v-list-item :to="{ name: 'categoria' }">
+              <v-list-item-action>
+                <v-icon> table_chart </v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title> Categorías </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          
+          
+          
+          </v-list-group>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Merca Ropa</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-footer height="auto">
+      <v-layout justify-center>
+        <v-flex text-xs-center>
+          <v-card flat tile color="primary" class="white--text">
+            <v-card-text class="white--text pt-0">
+              IncanatoIT &copy;2019
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+    drawer: true,
+  }),
+};
+</script>
